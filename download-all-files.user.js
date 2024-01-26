58037afecb9f9174bd6bd1b4761323f166c544cb
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-01-26 14:13:08 +0100

    Update download-all-files.user.js

--- download-all-files.user.js
+++ download-all-files.user.js
@@ -1,40 +1,40 @@
 // ==UserScript==
 // @name         Moodle File Downloader
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-//
+// @version      0.5
 // @description  Download files from Moodle and create a zip archive with progress bar
 // @author       PianoNic
-//
 // @downloadURL https://github.com/BBBaden-Moodle-userscripts/Download-All-Files/raw/main/download-all-files.user.js
 // @updateURL   https://github.com/BBBaden-Moodle-userscripts/Download-All-Files/raw/main/download-all-files.user.js
 // @homepageURL https://github.com/BBBaden-Moodle-userscripts/Download-All-Files
 // @supportURL  https://github.com/BBBaden-Moodle-userscripts/Download-All-Files/issues
-//
 // @match        https://moodle.bbbaden.ch/course/view.php*
 // @grant        GM_download
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @icon         https://github.com/BBBaden-Moodle-userscripts/Download-All-Files/blob/main/ico/icon.png?raw=true
 // ==/UserScript==
 
-(function () {
+(async function () {
     'use strict';
 
-    function sanitizeFilename(filename) {
-        const illegalCharsRegex = /[\\/:"*?<>|]/g;
-        const sanitizedFilename = filename.replace(illegalCharsRegex, '');
-        return sanitizedFilename.replace(/ /g, '_');
-    }
-
-    function downloadFile(url) {
-        return fetch(url)
-            .then(response => {
+    async function fetchFile(url, retries = 2) {
+        while (retries > 0) {
+            try {
+                const response = await fetch(url);
                 if (response.ok) {
-                    return { response, blob: response.blob() };
+                    const blob = await response.blob();
+                    return { response, blob };
                 } else {
                     throw new Error(`Failed to download file. Status code: ${response.status}`);
                 }
-            });
+            } catch (error) {
+                console.error(error, `Retrying... (Retries left: ${retries})`, "At Downloadlink:", url);
+                retries -= 1;
+            }
+        }
+
+        console.error("No more retries. Skipping...", "At Downloadlink:", url);
+        throw new Error("Failed to download file.");
     }
 
     function createProgressBar() {
@@ -71,11 +71,20 @@
 
         const progressBar = createProgressBar();
 
-        downloadButton.addEventListener('click', () => {
+        downloadButton.addEventListener('click', async () => {
             callback();
             // Hide the button after clicking to prevent multiple clicks
             downloadButton.style.display = 'none';
             cardTextDiv.appendChild(progressBar);
+
+            await new Promise(resolve => {
+                const interval = setInterval(() => {
+                    if (progressBar.value === 100) {
+                        clearInterval(interval);
+                        resolve();
+                    }
+                }, 100);
+            });
         });
 
         const reportDiv = document.createElement('div');
@@ -90,36 +99,34 @@
         return section;
     }
 
-    function generateZipAndDownload(zip, sanitizedTitle) {
-        zip.generateAsync({ type: 'blob' })
-            .then(blob => {
-                const link = document.createElement('a');
-                link.href = URL.createObjectURL(blob);
-                link.download = sanitizedTitle + '.zip';
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
+    async function generateZipAndDownload(zip, sanitizedTitle) {
+        const blob = await zip.generateAsync({ type: 'blob' });
 
-                // Hide the download button and progress bar
-                const downloadButton = document.querySelector('.btn.btn-outline-secondary.btn-sm');
-                const progressBar = document.getElementById('download-progress');
-                if (downloadButton) {
-                    downloadButton.style.display = 'none';
-                }
-                if (progressBar) {
-                    progressBar.style.display = 'none';
-                }
-            });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = sanitizedTitle + '.zip';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        // Hide the download button and progress bar
+        const downloadButton = document.querySelector('.btn.btn-outline-secondary.btn-sm');
+        const progressBar = document.getElementById('download-progress');
+        if (downloadButton) {
+            downloadButton.style.display = 'none';
+        }
+        if (progressBar) {
+            progressBar.style.display = 'none';
+        }
     }
 
-    function main() {
+    async function main() {
         const h1Tag = document.querySelector('div.page-header-headings h1.h2');
         const title = h1Tag.textContent.trim();
-        const sanitizedTitle = sanitizeFilename(title);
+        const sanitizedTitle = title.replace(/[\\/:"*?<>|]/g, '').replace(/ /g, '_');
 
-        const activityDivs = document.querySelectorAll('div.activityname');
+        const activityDivs = document.querySelectorAll('div.activity-basis.d-flex.align-items-center');
         const zip = new JSZip();
-        const downloadPromises = [];
         const totalFiles = activityDivs.length;
         let filesDownloaded = 0;
 
@@ -129,95 +136,84 @@
         const successfulDownloads = [];
         const failedDownloads = [];
 
-        function downloadWithRetry(href, retries = 2) {
-            const statusSpan = document.createElement('span');
+        // Function to create a badge element asynchronously
+        async function createBadgeAsync(color, message) {
+            var badge = document.createElement("span");
 
-            function attemptDownload() {
-                downloadFile(href)
-                    .then(({ response, blob }) => {
-                        const contentHeader = response.headers.get('content-disposition');
-                        const fileName = contentHeader.match(/filename="(.+)"/)[1];
-                        zip.file(fileName, blob);
-                        statusSpan.textContent = ' "✅ Successfully added to Archive"';
-                        successCount += 1;
-                        successfulDownloads.push({ fileName, href });
-                    })
-                    .catch(error => {
-                        statusSpan.textContent = ` "❌ Failed adding to Archive" (Retries left: ${retries})`;
+            badge.className = "badge badge-pill";
+            badge.style.backgroundColor = color;
 
-                        if (retries > 0) {
-                            console.error(error, "Retrying...", "At Downloadlink:", href);
-                            retries -= 1;
-                            attemptDownload();
-                        } else {
-                            console.error(error, "No more retries. Skipping...", "At Downloadlink:", href);
-                            failureCount += 1;
-                            failedLinks.push(href);
-                            failedDownloads.push(href);
-                        }
-                    })
-                    .finally(() => {
-                        filesDownloaded += 1;
-                        updateProgressBar((filesDownloaded / totalFiles) * 100);
-                    });
-            }
+            var strong = document.createElement("strong");
 
-            attemptDownload();
-            return statusSpan;
+            strong.textContent = message;
+
+            badge.appendChild(strong);
+
+            return badge;
         }
 
-        activityDivs.forEach((div, index) => {
+        async function downloadWithRetry(href) {
+            var badgeDiv;
+
+            try {
+                const { response, blob } = await fetchFile(href);
+                const contentHeader = response.headers.get('content-disposition');
+                const fileName = contentHeader.match(/filename="(.+)"/)[1];
+                zip.file(fileName, blob);
+                badgeDiv = await createBadgeAsync("green", "Erfolgreiches Speichern im Archiv!");
+                successCount += 1;
+                successfulDownloads.push({ fileName, href });
+            } catch (error) {
+                badgeDiv = await createBadgeAsync("red", "Fehler beim Speichern im Archiv!");
+                failureCount += 1;
+                failedLinks.push(href);
+                failedDownloads.push(href);
+            } finally {
+                filesDownloaded += 1;
+                updateProgressBar((filesDownloaded / totalFiles) * 100);
+            }
+
+            return badgeDiv;
+        }
+
+        for (const div of activityDivs) {
             const anchor = div.querySelector('a.aalink.stretched-link');
             if (anchor) {
                 const href = anchor.getAttribute('href');
                 if (href.includes('mod/resource')) {
-                    const statusSpan = downloadWithRetry(href);
-                    anchor.appendChild(statusSpan);
-                    downloadPromises.push(statusSpan);
+                    const status = await downloadWithRetry(href);
+                    div.appendChild(status);
                 }
             }
-        });
-
-        Promise.all(downloadPromises.map(statusSpan => {
-            return new Promise(resolve => {
-                const interval = setInterval(() => {
-                    if (statusSpan.textContent.includes('✅') || statusSpan.textContent.includes('❌')) {
-                        clearInterval(interval);
-                        resolve();
-                    }
-                }, 100);
-            });
-        }))
-        .then(() => {
-            generateZipAndDownload(zip, sanitizedTitle);
-            const reportSection = document.getElementById('report');
-
-            const successCountElement = document.createElement('p');
-            successCountElement.textContent = `Success Count: ${successCount}`;
-
-            const failureCountElement = document.createElement('p');
-            failureCountElement.textContent = `Failure Count: ${failureCount}`;
-
-            const failedLinksElement = document.createElement('ul');
-            failedLinksElement.innerHTML = failedLinks.map(link => `<li><a href="${link}" target="_blank">${link}</a></li>`).join('');
-
-            reportSection.appendChild(successCountElement);
-            reportSection.appendChild(failureCountElement);
-            reportSection.appendChild(failedLinksElement);
-
-            // Prepare data for console.table
-            const tableData = [
-                { 'Status': 'Success', 'Count': successCount },
-                { 'Status': 'Failed', 'Count': failureCount }
-            ];
-
-            // Display table in the console for successful and failed downloads
-            console.table(tableData);
-        });
         }
 
-        const asideElement = document.getElementById('block-region-side-pre');
-        if (asideElement) {
-            asideElement.appendChild(createDownloadButton(main));
-        }
+        generateZipAndDownload(zip, sanitizedTitle);
+
+        const reportSection = document.getElementById('report');
+
+        const successCountElement = document.createElement('p');
+        successCountElement.textContent = `Success Count: ${successCount}`;
+
+        const failureCountElement = document.createElement('p');
+        failureCountElement.textContent = `Failure Count: ${failureCount}`;
+
+        const failedLinksElement = document.createElement('ul');
+        failedLinksElement.innerHTML = failedLinks.map(link => `<li><a href="${link}" target="_blank">${link}</a></li>`).join('');
+
+        reportSection.appendChild(successCountElement);
+        reportSection.appendChild(failureCountElement);
+        reportSection.appendChild(failedLinksElement);
+
+        // Display table in the console for successful and failed downloads
+        console.table([
+            { 'Status': 'Success', 'Count': successCount },
+            { 'Status': 'Failed', 'Count': failureCount }
+        ]);
+    }
+
+    const asideElement = document.getElementById('block-region-side-pre');
+    if (asideElement) {
+        asideElement.appendChild(createDownloadButton(main));
+    }
+  console.log("Loaded Moodle Files Downloader!")
 })();
